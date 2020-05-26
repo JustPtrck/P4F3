63db29b80ab2dd4b9282fb528178ce54ff0dc6a9
Patrick Verwimp <patrickverwimp@gmail.com>
2020-05-26 12:28:36 +0200

    srdf updated test behavior updated

--- ariac_behaviors/ariac_flexbe_behaviors/src/ariac_flexbe_behaviors/test_sm.py
+++ ariac_behaviors/ariac_flexbe_behaviors/src/ariac_flexbe_behaviors/test_sm.py
@@ -9,8 +9,7 @@
 
 from flexbe_core import Behavior, Autonomy, OperatableStateMachine, ConcurrencyContainer, PriorityContainer, Logger
 from ariac_flexbe_states.start_assignment_state import StartAssignment
-from ariac_flexbe_states.detect_first_part_camera_ariac_state import DetectFirstPartCameraAriacState
-from flexbe_states.log_state import LogState
+from ariac_flexbe_states.srdf_state_to_moveit_ariac_state import SrdfStateToMoveitAriac
 # Additional imports can be added inside the following tags
 # [MANUAL_IMPORT]
 
@@ -51,6 +50,11 @@ class TestSM(Behavior):
 		_state_machine.userdata.camera_topic = 'ariac/logical_camera_bins2'
 		_state_machine.userdata.ref_frame = 'torso_base_main_joint'
 		_state_machine.userdata.part = ''
+		_state_machine.userdata.move_group_prefix = '/ariac/gantry'
+		_state_machine.userdata.move_group = ''
+		_state_machine.userdata.action_topic = '/move_group'
+		_state_machine.userdata.robot_name = ''
+		_state_machine.userdata.config_name = 'Gantry_Home'
 
 		# Additional creation code can be added inside the following tags
 		# [MANUAL_CREATE]
@@ -62,21 +66,15 @@ class TestSM(Behavior):
 			# x:30 y:40
 			OperatableStateMachine.add('start',
 										StartAssignment(),
-										transitions={'continue': 'scan'},
+										transitions={'continue': 'MOve'},
 										autonomy={'continue': Autonomy.Off})
 
-			# x:148 y:149
-			OperatableStateMachine.add('scan',
-										DetectFirstPartCameraAriacState(part_list=[pulley_part, gear_part_blue, gear_part_red], time_out=0.5),
-										transitions={'continue': 'log', 'failed': 'failed', 'not_found': 'failed'},
-										autonomy={'continue': Autonomy.Off, 'failed': Autonomy.Off, 'not_found': Autonomy.Off},
-										remapping={'ref_frame': 'ref_frame', 'camera_topic': 'camera_topic', 'camera_frame': 'camera_frame', 'part': 'part', 'pose': 'pose'})
-
-			# x:30 y:102
-			OperatableStateMachine.add('log',
-										LogState(text=part, severity=Logger.REPORT_INFO),
-										transitions={'done': 'finished'},
-										autonomy={'done': Autonomy.Off})
+			# x:164 y:116
+			OperatableStateMachine.add('MOve',
+										SrdfStateToMoveitAriac(),
+										transitions={'reached': 'finished', 'planning_failed': 'failed', 'control_failed': 'failed', 'param_error': 'failed'},
+										autonomy={'reached': Autonomy.Off, 'planning_failed': Autonomy.Off, 'control_failed': Autonomy.Off, 'param_error': Autonomy.Off},
+										remapping={'config_name': 'config_name', 'move_group': 'move_group', 'move_group_prefix': 'move_group_prefix', 'action_topic': 'action_topic', 'robot_name': 'robot_name', 'config_name_out': 'config_name_out', 'move_group_out': 'move_group_out', 'robot_name_out': 'robot_name_out', 'action_topic_out': 'action_topic_out', 'joint_values': 'joint_values', 'joint_names': 'joint_names'})
 
 
 		return _state_machine
